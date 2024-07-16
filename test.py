dd9fe3f865a6d239310262641dd659bd8ab0bc34
Mustapha Ait Ichou <66913451+aitichomo@users.noreply.github.com>
2024-07-16 10:53:32 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from keras.models import load_model
 
-model=load_model('C:/Users/Mustapha/Downloads/Facial Emotion/emotion_model_collab.h5')
+model=load_model('C:/Users/Mustapha/Downloads/Facial Emotion/moedl.h5')
 
 video=cv2.VideoCapture(0)
 
